b88c899381c7f9471217440d45c9bcb729a00adf
Пользователь <@DESKTOP-E7N1RLV>
2023-12-26 19:46:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Вводим коффиценты квадратного уравнения ");
+            //Console.Write("Введите коэффициент a: ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Введите коэффициент b: ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Введите коэффициент c: ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+
+            //double D = Math.Pow(b, 2) - 4 * a * c;//вычисления Дискриминанта
+
+            //if (D > 0)
+            //{
+            //    Console.WriteLine("Квадратное уравнение  имеет 2 корня");
+            //    double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+            //    double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+            //    Console.WriteLine($"Уравнение имеет два действительных корня: x1 = {x1:f2},\t x2 = {x2:f2}");
+            //}
+            //else if (D == 0)
+            //{
+            //    double x = -b / (2 * a);
+            //    Console.WriteLine($"Уравнение имеет один действительный корень: x = {x:f2}");
+            //    if (D < 0)
+            //    {
+            //        Console.WriteLine("Квадратное уравнение не имеет корней");
+            //    }
+            //}
+
+            //    Console.Write("Введите значение стороны a: ");
+            //    double a = double.Parse(Console.ReadLine());
+
+            //    Console.Write("Введите значение стороны b: ");
+            //    double b = double.Parse(Console.ReadLine());
+
+            //    Console.Write("Введите значение стороны c: ");
+            //    double c = double.Parse(Console.ReadLine());
+
+            //    if (a < b + c && b < a + c && c < a + b)
+            //    {
+            //        Console.WriteLine("Треугольник существует");
+
+            //        double p = (a + b + c) / 2;
+            //        double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            //        Console.WriteLine($"Площадь треугольника равна {S}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Треугольник не существует");
+            //}    Console.ReadKey();
+        }
+    }
+}
